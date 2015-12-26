1b02e2e5d3c55dfe162e7d649dd8f2ecedbd6bdd
unknown <Jmartin@PDX1CLILT091.na.ccli.net>
2015-12-26 12:21:02 -0800

    Refactoring.  Want to make sure I can recover.

--- run_analysis.R
+++ run_analysis.R
@@ -1,41 +1,108 @@
 
-################################################################################
-###	General Instructions for scritp.
-###
-###	REMOVE THIS HEADER BEFORE FINAL SUBMISSION
-###
-###	You should create one R script called run_analysis.R that does the following.
-###		- Merges the training and the test sets to create one data set.
-###		- Extracts only the measurements on the mean and standard deviation for
-###		  each measurement. 
-###		- Uses descriptive activity names to name the activities in the data set
-###		- Appropriately labels the data set with descriptive variable names. 
-###		- From the data set in step 4, creates a second, independent tidy
-###		  data set with the average of each variable for each activity and
-###		  each subject.
-
-
 
 ################################################################################
-###	Load the Test data set and organize it
+###		Load Feature labels and Activity Type Descriptions that are common
+###		to both the test data and the training data
 
-###	Read in the Subject data
-testSubj <- read.csv( "./data/test/subject_test.txt", col.names = c("SubjectID") )
+##	Feature labels
+featureLbls <- read.table( "./data/features.txt", col.names =  c("FeatureColumn", "FeatureName"), stringsAsFactors = FALSE )
+	
+#	Cleanse the feature label descriptive names until they can be used as column names in R
+for( i in 1:nrow(featureLbls) )
+{
+	##	Strip the invalid characters out of the feature name
+	featureLbls[i,]$FeatureName <- as.character( gsub( "\\(", "-", featureLbls[i,]$FeatureName ) )
+	featureLbls[i,]$FeatureName <- as.character( gsub( "\\)", "-", featureLbls[i,]$FeatureName ) )
+	featureLbls[i,]$FeatureName <- as.character( gsub( ",", "-", featureLbls[i,]$FeatureName ) )
+	featureLbls[i,]$FeatureName <- as.character( gsub( "-+", "-", featureLbls[i,]$FeatureName ) )
+	featureLbls[i,]$FeatureName <- as.character( gsub( "-$", "", featureLbls[i,]$FeatureName ) )
+}
 
-###	Read in the Feature data
+	
+##	Activity descriptions
+activityDesc <- read.table( "./data/activity_labels.txt", col.names = c("ActivityTypeID", "ActivityType"))
+
+
+
+
+
+################################################################################
+###		Load the Test data set and organize it
+
+
+###		Read in the various Test data files
+
+##	Subject data
+testSubj <- read.table( "./data/test/subject_test.txt", col.names = c("SubjectID") )
+
+##	Feature data
 testFeatures <- read.table( "./data/test/X_test.txt")
+names(testFeatures) <- featureLbls$FeatureName
 
-###	Read in the Activity labels
+##	Activity data
 testActivities <- read.table( "./data/test/y_test.txt", col.names = c("ActivityTypeID"))
+testActivities <- merge( testActivities, activityDesc, by.x = "ActivityTypeID", by.y = "ActivityTypeID")
 
-###	Read in the Feature labels
-testFeatureLbls <- read.table( "./data/features.txt", col.names = c("FeatureColumn", "FeatureName") )
+
+###		Combine data together by columns
+testData <- cbind( testSubj, testActivities, testFeatures )
 
 
 
 
 ################################################################################
-###	Load the Train data set and organize it
+###		Load the Train data set and organize it
+
+
+###		Read in the various Train data files
+
+##	Subject data
+trainSubj <- read.table( "./data/train/subject_train.txt", col.names = c("SubjectID") )
+
+##	Feature data
+trainFeatures <- read.table( "./data/train/X_train.txt")
+names(trainFeatures) <- featureLbls$FeatureName
+
+
+##	Activity data
+trainActivities <- read.table( "./data/train/y_train.txt", col.names = c("ActivityTypeID"))
+trainActivities <- merge( trainActivities, activityDesc, by.x = "ActivityTypeID", by.y = "ActivityTypeID")
+
+
+###		Combine data together by columns
+trainData <- cbind( trainSubj, trainActivities, trainFeatures )
+
+
+
+
+################################################################################
+###		Combine the Training and Testing data together to generate the
+###		Experiment Data.
+
+expData <- rbind( trainData, testData )
+
+
+
+
+################################################################################
+###		Filter Experiment Data (expData) down to the columns we need.
+
+###	Identify the columns that hold mean/std values
+m1 <- names(expData)[grep("\\-mean\\-", names(expData), ignore.case = TRUE )]
+m2 <- names(expData)[grep("\\-mean$", names(expData), ignore.case = TRUE )]
+s1 <- names(expData)[grep("\\-std\\-", names(expData), ignore.case = TRUE )]
+s2 <- names(expData)[grep("\\-std$", names(expData), ignore.case = TRUE )]
+ms <- c( "SubjectID", "ActivityType", m1, m2, s1, s2 )
+desiredColumns <- names(expData) %in% ms
+
+###	Filter expData down to columns we want to work with
+expData <- expData[, desiredColumns]
+
+
+
+
+
+
 
 
 
